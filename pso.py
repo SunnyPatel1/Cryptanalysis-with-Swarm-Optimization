c53c3357d5f9aaa7301e03795553ac6d41b114d6
Samantha Husack <31865603+booklover98@users.noreply.github.com>
2018-11-20 19:06:12 -0500

    commented code

--- pso.py
+++ pso.py
@@ -5,37 +5,52 @@ import numpy as np
 
 class Particle:
     def __init__(self, bounds):
-        self.position_i=[]
-        self.velocity_i=[]
-        self.pos_best_i=[]
-        self.err_best_i=-1
-        self.err_i=-1
+        # Initalize particle variables
+        self.position_i=[]      # particle position
+        self.velocity_i=[]      # particle velocity
+        self.pos_best_i=[]      # particle personal best
+        self.err_best_i=-1      # particle error best
+        self.err_i=-1           # particle error
 
+        # For every dimension, add particle and initiate velocity and position
         for i in range(0, num_dimensions):
             self.velocity_i.append(random.uniform(-1,1))
             self.position_i.append(random.uniform(bounds[i][0], bounds[i][1]))
 
+    # Evaluate position in comparison to personal best and global best
     def evaluate(self, costFunc):
         self.err_i=costFunc(self.position_i)
-
+        
+        # check particle error is less than error best or if error best is =-1
         if self.err_i < self.err_best_i or self.err_best_i == -1:
-            self.pos_best_i=self.position_i
-            self.err_best_i=self.err_i
+            self.pos_best_i=self.position_i     # set personal best to current position
+            self.err_best_i=self.err_i          # set error best to particle current error
+            
+    # Update particle velocity
     def update_velocity(self, pos_best_g):
-        w=0.9
-        c1=2.05
-        c2=2.05
+        w=0.9       # intertia weight
+        c1=2.05     # self-confidence
+        c2=2.05     # swarm-confidence
 
+        # For each dimension
         for i in range(0, num_dimensions):
-            r1 = random.random()
-            r2 = random.random()
+            r1 = random.random()    # uniformly generated random num range[0,1] 
+            r2 = random.random()    # uniformly generated random num range[0,1]
 
+        # Set Velocity using cognitive and social velocities. 
         vel_cognitive = c1*r1*(self.pos_best_i[i]-self.position_i[i])
         vel_social=c2*r2*(pos_best_g[i]-self.position_i[i])
+        
+        # Vi^(t+1) = ( w * Vi^(t) ) + [(C1 * r1) * (pBesti - X1^(t))] + [(C2 * r2) * (gBesti - Xi^(t))]
         self.velocity_i[i]=w*self.velocity_i[i]+vel_cognitive+vel_social
 
+    # Update particle position
     def update_position(self, bounds):
+        
+        # For each dimension
         for i in range(0,num_dimensions):
+            
+            # Xi^)t+1) = Xi^(t) + Vi^(t+1)
             self.position_i[i]=self.position_i[i]+self.velocity_i[i]
 
             # adjust maximum position if necessary
@@ -45,11 +60,12 @@ class Particle:
             # adjust minimum position if neseccary
             if self.position_i[i] < bounds[i][0]:
                 self.position_i[i]=bounds[i][0]
+                                
 class PSO():
     def __init__(self,costFunc,x0,bounds,num_particles,maxiter):
-        global num_dimensions
+        global num_dimensions           # define global variable for number of positions
 
-        num_dimensions=size
+        num_dimensions=size             # set the number of positions to be the size of key
         err_best_g=-1                   # best error for group
         pos_best_g=[]                   # best position for group
 
@@ -60,6 +76,8 @@ class PSO():
 
         # begin optimization loop
         i=0
+        
+        # while less than the maximum number of set iterations
         while i < maxiter:
             print("On iteration " + str(i) + " of " + str(maxiter))
             #print i,err_best_g
@@ -83,6 +101,7 @@ class PSO():
         print(vt.toString([ int(x) for x in pos_best_g ]))
         print(err_best_g)
 
+# define fitness function
 def lossFunc(positions):
     encryptedNums = vt.toNumArray(encrypted)
     key = [int(x) for x in positions]
@@ -95,11 +114,12 @@ def lossFunc(positions):
 #Will later do this with kasinski (or similar name) method
 size = int(input("Enter size of key: "))
 
+# open encrypted text from text file
 toRead = open('encrypted.txt', 'r')
 encrypted = toRead.read()
 toRead.close()
 
-
+# set bounds and decrypt.
 bounds=np.tile([(0,25)], (size,1))
 PSO(lossFunc, size, bounds, num_particles=100, maxiter=100)
 
