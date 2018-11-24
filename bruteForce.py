e019e590a23629fdf97d22cae8d69daeceee1bd4
Thomas Jansz <thomas.jansz@uoit.net>
2018-11-24 02:03:51 -0500

    bruteforce code

--- bruteForce.py
+++ bruteForce.py
@@ -1,5 +1,27 @@
 import vigenereTools as vt
-import numpy as np
+import itertools
 
+min = -1
+min_decrypt_arr = []
+best_key = ''
+inp = input("Enter key character length to test: ")
+key_length = int(inp)
+inp = input("Enter cipher: ")
+cipher = inp
+cipher_arr = vt.toNumArray(cipher)
 
-def matchingWords
+for i in range(1, key_length + 1):
+    generator=itertools.combinations_with_replacement('ABCDEFGHIJKLMNOPQRSTUVWXYZ', i )
+    for password in generator:
+        key = ''.join(password)
+        key_arr = vt.toNumArray(key)
+        keyLong = vt.extendCipherText(key_arr, int(len(cipher_arr)/len(key)), len(cipher_arr))
+        decrypted = vt.decrypt(cipher_arr, keyLong)
+        fitness = vt.getFitness(decrypted)
+        if (min == -1 or min > fitness):
+            min = fitness
+            min_decrypt_arr = decrypted
+            best_key = key
+
+    print(vt.toString(min_decrypt_arr))
+    print('KEY: ' + best_key)
