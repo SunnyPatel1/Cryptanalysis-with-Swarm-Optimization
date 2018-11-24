167bc64c587008c8e97438d74fb2b5131a10e5ae
Samantha Husack <31865603+booklover98@users.noreply.github.com>
2018-11-24 15:33:20 -0500

    added comments

--- bruteForce.py
+++ bruteForce.py
@@ -1,17 +1,39 @@
-import vigenereTools as vt
-import itertools
+import vigenereTools as vt      # Import Common Tools
+import itertools                # Import
 
+# Initiate Variables: min fitness function, best decryption, and best key
 min = -1
-min_decrypt_arr = []
+min_decrypt_arr = []    
 best_key = ''
+
+#Get Key length from user
 inp = input("Enter key character length to test: ")
 key_length = int(inp)
-inp = input("Enter cipher: ")
-cipher = inp
+
+# Get cipher text from user
+# inp = input("Enter cipher: ")
+# cipher = inp
+
+# OR
+# Get cipher text from file
+toRead = open('encrypted.txt', 'r')
+cipher = toRead.read()
+toRead.close()
+
+# Assign cipher text to array
 cipher_arr = vt.toNumArray(cipher)
 
+# For the length of the key
 for i in range(1, key_length + 1):
+    # Generate all combinations with replacement of given key length
     generator=itertools.combinations_with_replacement('ABCDEFGHIJKLMNOPQRSTUVWXYZ', i )
+    
+    # For each possible key combo...
+    #   Decrypt using that key
+    #   Get fitness function for that decrypted text
+    #   Assign that fitness as new best if higher than all previous
+    #   When done, best fitness test results are printed to user 
+    #       (i.e. best key, decrypted text and fitness function)
     for password in generator:
         key = ''.join(password)
         key_arr = vt.toNumArray(key)
@@ -23,6 +45,7 @@ for i in range(1, key_length + 1):
             min_decrypt_arr = decrypted
             best_key = key
 
+# Print results to user
 print('\nOutput: ')
 print(vt.toString(min_decrypt_arr))
 print('KEY: ' + best_key)
